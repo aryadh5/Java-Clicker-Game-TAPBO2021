5662574f923958fd7e38aae3eb24b936726f5024
aryadh5 <119779571+aryadh5@users.noreply.github.com>
2022-12-18 19:17:56 +0700

    Delete gamepbo directory

--- gamepbo/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.gamepbo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.gamepbo to javafx.fxml;
-    exports com.example.gamepbo;
-}
